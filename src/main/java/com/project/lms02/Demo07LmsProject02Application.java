769b71901f4a67f56de53c30b3cb8fe0b0398b62
rememberfla <ecaorez@gmail.com>
2023-07-18 14:39:51 +0900

    setting_base

---
+++ src/main/java/com/project/lms02/Demo07LmsProject02Application.java
@@ -0,0 +1,13 @@
+package com.project.lms02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo07LmsProject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo07LmsProject02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/lms02/Demo07LmsProject02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lms02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo07LmsProject02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
